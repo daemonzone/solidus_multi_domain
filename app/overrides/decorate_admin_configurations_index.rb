722db6ed8d01da812cf50eb613f32a4e8c5c1f48
Davide <davide@daemonzone.it>
2021-11-08 16:07:38 +0100

    + Stores tab have been corrected + Edit store view has been merged with the spree default one

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -3,7 +3,7 @@
 Deface::Override.new(
   virtual_path: "spree/admin/shared/_configuration_menu",
   name: "multi_domain_admin_configurations_menu",
-  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
+  replace: "[data-hook='admin_settings_stores']",
   disabled: false,
   partial: "spree/shared/multi_domain_sidebar_entry"
 )
